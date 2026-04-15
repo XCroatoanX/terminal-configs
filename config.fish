1154f2933cf862d1403ebb67163cb90d600065d3
Artem Stasyuk <stasyukartem2@gmail.com>
2026-04-15 22:05:21 +0200

    Update fish greeting and prompt configuration

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-set -U fish_greeting "I use Arch, btw"
+set -g fish_greeting ""
 # Run only in interactive sessions
 if status is-interactive
     # Commands for interactive shells go here
@@ -7,7 +7,7 @@ end
 # macOS-style prompt: "username@hostname ~ %"
 function fish_prompt
     set_color normal
-    echo -n (whoami)"@"(hostname -s)" "(prompt_pwd)" % "
+    echo -n (whoami)" "(prompt_pwd)" % "
 end
 
 # Abbreviations (aka aliases)
@@ -18,3 +18,19 @@ abbr -a -- install 'sudo apt-get install'
 abbr -a -- update 'sudo apt update && sudo apt upgrade -y'
 abbr -a -- fullupdate 'sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y'
 abbr -a -- sysinfo 'neofetch'
+
+# opencode
+fish_add_path /home/arst/.opencode/bin
+
+abbr -a -- lg 'lazygit'
+
+if status is-interactive
+    # Terraform Abbreviations
+    abbr -a tfi terraform init
+    abbr -a tfp terraform plan
+    abbr -a tfa terraform apply
+    abbr -a tfd terraform destroy
+    abbr -a tfv terraform validate
+end
+
+abbr -a -- c 'clear'
